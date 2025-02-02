56e422fa6caaee21f47b0077c15801b07d28383e
Luis Velasquez <l.velasquez1308@gmail.com>
2025-02-01 21:45:28 -0600

    arreglado bug con la formacion del jefe

--- main/scripts/formations.lua
+++ main/scripts/formations.lua
@@ -2,7 +2,7 @@ local squads = require("main/scripts/squads")
 
 return {
     [1] = {
-            -- squads.side_by_side_5_fighter,
+            squads.side_by_side_5_fighter,
             -- squads.side_by_side_5_fighter_powerup_other,
             -- squads.h_formation_7_scout_powerup,
             -- squads.v_formation_9_fighter,
@@ -10,7 +10,10 @@ return {
             -- squads.diagonal_left_5_scout,
             -- squads.diagonal_right_5_scout,
             -- squads.diagonal_left_5_fighter_diagonal_right_5_fighter,
-            squads.boss_1
+            squads.boss_1,
+            squads.side_by_side_5_fighter,
+            squads.side_by_side_5_fighter,
+            squads.side_by_side_5_fighter,
         
     },
     [2] = {
