77ed4da459d247c6e79df433ce592afb0ec3af98
Luis Velasquez <l.velasquez1308@gmail.com>
2025-02-05 15:45:33 -0600

    agregado gui de arma

--- main/scripts/weapons/bounty_hunter_weapon.lua
+++ main/scripts/weapons/bounty_hunter_weapon.lua
@@ -4,6 +4,7 @@ local BountyHunterWeapon = Weapon:new()
 
 function BountyHunterWeapon:new(factory_url)
     local instance = Weapon.new(self, factory_url, 10, 400, 0.5, "/bounty_hunter_bullet") -- Personaliza propiedades
+    instance.avatar = "bounty_hunter_bullet"
     setmetatable(instance, { __index = self })
     return instance
 end
