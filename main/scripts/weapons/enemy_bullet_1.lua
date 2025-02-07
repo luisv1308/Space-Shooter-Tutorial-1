f40a152c4bd7fb5f1097a13fcfcc0c72afae4a09
Luis Velasquez <l.velasquez1308@gmail.com>
2025-02-07 02:00:10 -0600

    agregado dano a player y efecto visual

--- main/scripts/weapons/enemy_bullet_1.lua
+++ main/scripts/weapons/enemy_bullet_1.lua
@@ -3,7 +3,7 @@ local Weapon = require "main.scripts.weapons.weapon"
 local EnemyBullet1 = Weapon:new()
 
 function EnemyBullet1:new(factory_url)
-    local instance = Weapon.new(self, factory_url, 1, 250, 1.2, "/enemy_bullet_1") -- Personaliza propiedades
+    local instance = Weapon.new(self, factory_url, 10, 250, 1.2, "/enemy_bullet_1") -- Personaliza propiedades
     setmetatable(instance, { __index = self })
     return instance
 end
